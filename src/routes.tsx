8ca37a74b4b0e9cdf8e27b712221f10260629b2b
Abdelrahman Mohamed <abdelrahman.mohamed1234@el-eng.menofia.edu.eg>
2025-09-16 01:01:21 +0300

    delete import footer

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import { Home } from "./pages/index";
-import Footer from "./components/layout/Footer";
 
 export const router = createBrowserRouter([
   {
