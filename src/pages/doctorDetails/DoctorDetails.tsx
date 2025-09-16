7bc676eb01305e320c77940a4fa5c75f9a73f03c
HagarRagab <101262480+HagarRagab@users.noreply.github.com>
2025-09-16 12:38:30 +0300

    Adding doctor details page layout

--- src/pages/doctorDetails/DoctorDetails.tsx
+++ src/pages/doctorDetails/DoctorDetails.tsx
@@ -1,6 +1,10 @@
-import GoBackButton from "@/components/common/GoBackButton";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Heart } from "lucide-react";
+import GoBackButton from "@/components/common/GoBackButton";
+import DoctorHeader from "./components/DoctorHeader";
+import DoctorStats from "./components/DoctorStats";
+import AboutDoctor from "./components/AboutDoctor";
+import DoctorReviews from "./components/DoctorReviews";
+import DoctorPricing from "./components/DoctorPricing";
 
 function DoctorDetails() {
     return (
@@ -13,20 +17,11 @@ function DoctorDetails() {
                 </button>
             </header>
 
-            <section className="flex items-center gap-4">
-                <Avatar className="w-24 h-24">
-                    <AvatarImage src="https://github.com/shadcn.png" />
-                    <AvatarFallback>DC</AvatarFallback>
-                </Avatar>
-
-                <div>
-                    <h2>Dr. Jessica Turner</h2>
-                    <p>Pulmonologist</p>
-                    <div>
-                        <p>129,El-Nasr Street, Cairo</p>
-                    </div>
-                </div>
-            </section>
+            <DoctorHeader />
+            <DoctorStats />
+            <AboutDoctor />
+            <DoctorReviews />
+            <DoctorPricing />
         </>
     );
 }
