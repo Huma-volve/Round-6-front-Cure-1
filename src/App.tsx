46ed93acb8b9bb27171ef4c254aec0be9745120e
Ziad El-Sayed <104182634+ZiadElsayed01@users.noreply.github.com>
2025-09-17 19:27:06 +0300

    Fix: remove get favourite from app

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,8 @@ import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import { Toaster } from "react-hot-toast";
-import { useEffect } from "react";
-import { handleGetFavorites } from "./api/favourite/favourite";
 
 function App() {
-  useEffect(() => {
-    (async () => {
-      const favorites = await handleGetFavorites();
-      console.log("Favorites:", favorites);
-    })();
-  }, []);
-
   return (
     <>
       <RouterProvider router={router} />
