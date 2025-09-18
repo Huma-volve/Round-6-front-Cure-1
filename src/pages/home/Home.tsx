5ea567f2e69f69be3244d6ba91b15a5f82d0f3df
Ziad El-Sayed <104182634+ZiadElsayed01@users.noreply.github.com>
2025-09-19 02:49:23 +0300

    Feat: implement FAQ section in home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import Map from "./components/Map";
 import TopRated from "./components/TopRated";
 import Reviews from "./components/Reviews";
+import FAQ from "./components/FAQ";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Map />
       <TopRated />
       <Reviews />
+      <FAQ />
     </>
   );
 }
---
+++ src/pages/home/components/FAQ.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+const faqData = [
+  {
+    id: 1,
+    question: "Is it safe to book online?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+  },
+  {
+    id: 2,
+    question: "Is the app free to use?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+  },
+  {
+    id: 3,
+    question: "How can I find a doctor?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+  },
+  {
+    id: 4,
+    question: "Can I cancel my appointment?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+  },
+  {
+    id: 5,
+    question: "What payment are supported?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+  },
+  {
+    id: 6,
+    question: "How do I edit my profile?",
+    answer:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+  },
+];
+
+export default function FAQ() {
+  return (
+    <>
+      <div className="w-full md:w-[60%] mx-auto">
+        <div className="py-1 px-5 text-primary bg-primary/10 rounded-full w-fit mx-auto">
+          Frequently Asked Questions
+        </div>
+        <h2 className="md:text-3xl text-xl font-semibold text-center mt-2 mb-10">
+          Got Questions ? We’ve got Answers!
+        </h2>
+
+        <Accordion type="single" collapsible>
+          {faqData.map((item) => (
+            <AccordionItem
+              value={`item-${item.id}`}
+              key={item.id}
+              className="bg-secondary-100 mb-2 px-5 border-none rounded-md"
+            >
+              <AccordionTrigger className="text-lg font-medium">
+                {item.question}
+              </AccordionTrigger>
+              <AccordionContent className="text-sm">
+                {item.answer}
+              </AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </div>
+    </>
+  );
+}
