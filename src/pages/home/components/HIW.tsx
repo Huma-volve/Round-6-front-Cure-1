b7dee6fff95d727690d0be42844e3a4d30581ad1
Ziad El-Sayed <104182634+ZiadElsayed01@users.noreply.github.com>
2025-09-19 04:43:38 +0300

    Fix Card Issue

--- src/pages/home/components/HIW.tsx
+++ src/pages/home/components/HIW.tsx
@@ -44,7 +44,7 @@ export default function HIW() {
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {cards.map((card) => (
-            <CardAction
+            <Card
               key={card.id}
               className="p-0 rounded-4xl overflow-hidden border border-black/40 z-40"
             >
